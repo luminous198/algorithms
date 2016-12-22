66b112d2a1bb3959556ecd980dd1372f58fae9b2
luminous198 <milangarg6991@gmail.com>
2016-12-22 20:23:39 +0530

    Made test functions in the main file

--- main_file.py
+++ main_file.py
@@ -2,7 +2,63 @@ from graph import Graph
 import graph_functions as graph_func
 import graph_functions_new as graph_func_new
 
+
+def testFunction(g):
+	#Tests the topological sort using DFS
+	topSort = graph_func.topologicalSort(g)
+	topSort = [x._element for x in topSort]
+	print("The topological sort of the graph is {0} ".format(topSort))
+	print("\n\n\n\n\n\n")
 	
+	#Tests the strongly connected components for graph
+	(SCC_forest,startTime,finishTime) = graph_func_new.stronglyConnectedComponents(g)	
+	print("The vertices with strongly connected compnents are : ")
+	for vertex in SCC_forest:
+		if SCC_forest[vertex] is None:
+			print(vertex._element)
+	print('\n\n\n')
+		
+	
+	#Tests the topological sort using iterative DFS
+	topSortIter = graph_func.topologicalSortIterative(g)
+	topSortIter = [x._element for x in topSortIter]
+	print("The topological sort of the graph using iterative DFS is {0} ".format(topSortIter))
+
+def printGraph(G,directed=False):
+	print("Print the graph.")
+	for vertex,adj in G._outgoing.items():
+		for neigh,value in adj.items():
+			print(vertex._element,neigh._element,value._element)
+	
+	if directed:
+		print("Print the reverse graph.")
+		for vertex,adj in G._incoming.items():
+			for neigh,value in adj.items():
+				print(vertex._element,neigh._element,value._element)
+	
+def insertEdgesList(G,edgeList):
+	'''
+		Takes a list of edges to be put in the graph.
+		edgeList contains tuples of information on edges.
+		First item in each tuple is originating vertex, Second item is the
+		destination vertex and third is the value for the edge.
+	'''
+	
+	for edge in edgeList:
+		G.insert_edge(edge[0],edge[1],edge[2])
+
+def showGraphInfo(G):
+	print("Number of vertices in the graph : {0}".format(g.vertex_count()))
+	vertex_list = []
+	for vertex in G.vertices():
+		vertex_list.append(vertex._element)
+	print("List of Vertces in the graph : {0}".format(vertex_list))
+	
+	print("Number of Edges in the graph: {0}".format(G.edge_count()))
+	print("List of edges in the graph")
+	for edge in G.edges():
+		print("{0} ----> {1} with value {2}".format(edge._origin._element,\
+				edge._destination._element,edge._element))
 if __name__ == "__main__":
 	
 	g = Graph(directed = True)
@@ -13,36 +69,14 @@ if __name__ == "__main__":
 	w = g.insert_vertex('w')
 	z = g.insert_vertex('z')
 	print("Number of vertices in the graph: {0}".format(g.vertex_count()))
-	g.insert_edge(u,v,2)
-	g.insert_edge(u,x,1)
-	g.insert_edge(x,v,9)
-	g.insert_edge(v,y,10)
-	g.insert_edge(y,x,11)
-	g.insert_edge(w,y,12)
-	g.insert_edge(w,z,13)
-	g.insert_edge(z,z,14)
-	print("Number of Edges in the graph: {0}".format(g.edge_count()))
-	print("List of edges in the graph")
-	for edge in g.edges():
-		print("{0} ----> {1} with value {2}".format(edge._origin._element,\
-				edge._destination._element,edge._element))
-	print("\n\n\n\n\n\n")
 	
-	topSort = graph_func.topologicalSort(g)
-	topSort = [x._element for x in topSort]
-	print("The topological sort of the graph is {0} ".format(topSort))
-	print("\n\n\n\n\n\n")
+	edgeList = [(u,v,2),(u,x,1),(x,v,9),(v,y,10),\
+				(y,x,11),(w,y,12),(w,z,13),(z,z,14)]
+	insertEdgesList(g,edgeList)
+	showGraphInfo(g)
 	
-	(SCC_forest,startTime,finishTime) = graph_func_new.stronglyConnectedComponents(g)	
-	print("The vertices with strongly connected compnents are : ")
-	for vertex in SCC_forest:
-		if SCC_forest[vertex] is None:
-			print(vertex._element)
-	print('\n\n\n')
-		
-	#for vertex in graph_func.tempSort(g):
-	#	print(vertex._element)
-
-	topSortIter = graph_func.topologicalSortIterative(g)
-	topSortIter = [x._element for x in topSortIter]
-	print("The topological sort of the graph using iterative DFS is {0} ".format(topSortIter))
+	
+	
+	
+	
+	
