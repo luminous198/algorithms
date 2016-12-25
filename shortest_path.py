ff4f353be805861a99b71e7fbd4432e62a4957f6
luminous198 <milangarg6991@gmail.com>
2016-12-25 14:04:53 +0530

    Added Shortest path tree.

--- shortest_path.py
+++ shortest_path.py
@@ -9,6 +9,14 @@ def djikstraShortestPath(G,source):
 		Graph must not contain negative cycles for the algorithm to work.
 		Return dictionary mapping each reachable vertex to its distance 
 		from src.
+		
+		d is a dictionary storing the distance of other vertices from 
+		the source vertex.
+		pQ is a priority Queue from which the vertex with the smallest 
+		distance is popped.
+		whenever a vertex is popped from the priority Queue it is added
+		to the dictionary cloud and all its adjacent edges which are not
+		in cloud are examined.
 	'''
 	
 	d = {}
@@ -18,6 +26,9 @@ def djikstraShortestPath(G,source):
 	
 	maxDistance = float("inf")
 	
+	# for each vertex v of the graph, add an entry to the priority 
+	# queue, with the source having distance 0 and all others having
+	# infinite distance.
 	for vertex in G.vertices():
 		if vertex is source:
 			d[vertex] = 0
@@ -44,10 +55,20 @@ def djikstraShortestPath(G,source):
 		for e in G.incident_edges(u):
 			v = e.opposite(u)
 			if v not in cloud:
+				# perform relaxation step on edge (u,v)
 				weight = e.element()
 				if d[u] + weight < d[v]:
-					d[v] = d[u] + weight
+					d[v] = d[u] + weight #update the distance
 					pQ.updateKey(v,d[v])
 	return cloud
 					
-		
+def shortes_path_tree(G,source,d):
+	tree = {}
+	for v in d:	
+		if v is not source:
+			for e in G.incident_edges(v,False):
+				u = e.opposite(v)
+				weight = e.element()
+				if d[v] == d[u] + weight:
+					tree[v] = e
+	return tree
